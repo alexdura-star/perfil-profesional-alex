0013235cf45f9ec661fb6494d3fd350d7c1dbd7a
alexdura-star <56644477+alexdura-star@users.noreply.github.com>
2020-04-19 20:06:23 +0200

    Add files via upload

--- send.php
+++ send.php
@@ -6,14 +6,13 @@ $poblacion = $_POST['poblacion'];
 $sexo=$_POST['GrupoOpciones1'];
 $radio= $_POST['GrupoOpciones2'];
 $opinion=$_POST['opinion'];
-$header = 'From: ' . $mail . ", de la poblacion ".$poblacion."\r\n";
+$header = 'From: ' . $mail . ", de la población ".$poblacion."\r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
 $mensaje = "Este mensaje fue enviado por " . $nombre . " \r\n";
 $mensaje .= "Su e-mail es: " . $mail . " \r\n";
-$mensaje .= "sexo" . $_POST['GrupoOpciones1'] . " \r\n";
 $mensaje .= "persona contacto" . $_POST['GrupoOpciones2'] . " \r\n";
 $mensaje .= "mensaje".$_POST['opinion'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
